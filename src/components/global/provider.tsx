38ff93a41be484ac122b0994257c10f4486ddfcf
Sarthak Kundu <74956651+dragoluca22@users.noreply.github.com>
2023-07-29 00:03:38 +0530

    fixed tooltip delay

--- src/components/global/provider.tsx
+++ src/components/global/provider.tsx
@@ -4,7 +4,7 @@ import { DefaultProps } from "@/src/types";
 import { TooltipProvider } from "../ui/tooltip";
 
 function Provider({ children }: DefaultProps) {
-    return <TooltipProvider>{children}</TooltipProvider>;
+    return <TooltipProvider delayDuration={0}>{children}</TooltipProvider>;
 }
 
 export default Provider;
